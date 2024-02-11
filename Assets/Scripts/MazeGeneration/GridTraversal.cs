6b8cc8466bf8fb03014e4cbf0610630e0e5af1fd
SimonL9889 <113265728+SimonL9889@users.noreply.github.com>
2024-02-11 15:36:22 -0500

    full but faulty maze

--- Assets/Scripts/MazeGeneration/GridTraversal.cs
+++ Assets/Scripts/MazeGeneration/GridTraversal.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using System.Linq;
 using System.Collections.Generic;
-using System.Linq;
+using UnityEngine;
 
 namespace ShareefSoftware
 {
@@ -16,11 +16,15 @@ namespace ShareefSoftware
          * Define your instance variables here
          */
         private bool[,] visited;
+        private readonly System.Random random;
+        private List<(int Row, int Column)> cellsVisited;
 
         /// Constructor
         public GridTraversal(IGridGraph<T> grid)
         {
             this.grid = grid;
+            random = new System.Random();
+            cellsVisited = new List<(int Row, int Column)>();
         }
 
         /*
@@ -32,35 +36,91 @@ namespace ShareefSoftware
         public IEnumerable<((int Row, int Column) From, (int Row, int Column) To)> GenerateMaze(int startRow, int startColumn)
         {
             visited = new bool[grid.NumberOfRows, grid.NumberOfColumns];
-            return PrimsTraverse((startRow, startColumn));
+            return PrimsEdgeSelect((startRow, startColumn));
             
         }
 
-        private IEnumerable<((int Row, int Column) From, (int Row, int Column) To)> PrimsTraverse((int Row, int Column) cell)
+        private IEnumerable<((int Row, int Column) From, (int Row, int Column) To)> PrimsEdgeSelect((int Row, int Column) cell)
         {
-            var nextStepNominees = new List<(int Row, int Column)>();
+            var vertexNominees = new List<(int Row, int Column)>();
+            var allNominees = new List<(int Row, int Column)>();
+
             if (!visited[cell.Row, cell.Column])
             {
                 visited[cell.Row, cell.Column] = true;
-                foreach (var neighbor in grid.Neighbors(cell.Row, cell.Column))
+                cellsVisited.Add((cell.Row, cell.Column));
+                Debug.Log("Row " + cell.Row + " Column " + cell.Column);
+
+                foreach (var frontierCell in cellsVisited)
                 {
-                    if (!visited[neighbor.Row, neighbor.Column])
+                    vertexNominees = createCutList(frontierCell);
+                    if (vertexNominees.Count != 0)
                     {
-                        nextStepNominees.Add(neighbor);
+                        foreach (var vertex in vertexNominees)
+                            allNominees.Add(vertex);
                     }
                 }
-                var random = new Random();
 
-                while (nextStepNominees.Count > 0)
+                // Randomly select an edge on the cut
+                // Handle the situation when not encountering deadend
+                if (vertexNominees.Count > 0)
                 {
-                    int rndIndex = random.Next(nextStepNominees.Count);
-                    var next = nextStepNominees[rndIndex];
-                    nextStepNominees.Remove(next);
+                    int rndIndex = random.Next(vertexNominees.Count);
+                    var next = vertexNominees[rndIndex];
+                    
+                    foreach (var edge in PrimsEdgeSelect(next))
+                    {
+                        yield return edge;
+                    }
 
+                    vertexNominees.Remove(next);
+                    //allNominees.Remove(next);
                     yield return ((cell.Row, cell.Column), next);
+                    
+                }
+                else if (allNominees.Count > 0)
+                {
+                    int rndIndex = random.Next(allNominees.Count);
+                    var next = allNominees[rndIndex];
+
+                    foreach (var edge in PrimsEdgeSelect(next))
+                    {
+                        yield return edge;
+                    }
+
+                    //vertexNominees.Remove(next);
+                    allNominees.Remove(next);
+
+                    int splitingCellIndex = 0;
+                    foreach(var nextNeighbor in grid.Neighbors(next.Row, next.Column))
+                    {
+                        for (int i = 0; i < cellsVisited.Count; i++)
+                        {
+                            if ((nextNeighbor.Row == cellsVisited[i].Row) && (nextNeighbor.Column == cellsVisited[i].Column))
+                            {
+                                splitingCellIndex = i;
+                            }
+                        }
+                    }
+
+                    yield return (cellsVisited[splitingCellIndex], next);
                 }
             }
         }
 
+        private List<(int Row, int Column)> createCutList((int Row, int Column) cell)
+        {
+            var nextStepNominees = new List<(int Row, int Column)>();
+
+            foreach (var neighbor in grid.Neighbors(cell.Row, cell.Column))
+            {
+                if (!visited[neighbor.Row, neighbor.Column])
+                {
+                    nextStepNominees.Add(neighbor);
+                }
+            }
+
+            return nextStepNominees;
+        }
     }
 }
