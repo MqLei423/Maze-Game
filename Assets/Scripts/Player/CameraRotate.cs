ea7fd306512e666e4f4300235262d76dc8a2cc7f
SimonL9889 <113265728+SimonL9889@users.noreply.github.com>
2024-02-26 17:54:11 -0500

    Camera fixed

--- Assets/Scripts/Player/CameraRotate.cs
+++ Assets/Scripts/Player/CameraRotate.cs
@@ -5,7 +5,7 @@ public class CameraRotate : MonoBehaviour
 {
     [SerializeField] private GameObject playerToMove;
     private InputAction input;
-    [SerializeField]private float sensitivity = 10f;
+    [SerializeField]private float sensitivity = 15f;
 
     public void Initialize(InputAction input)
     {
@@ -31,7 +31,8 @@ public class CameraRotate : MonoBehaviour
         // Try to set off the z rotation resulted in by above rotation
         if (playerToMove.transform.rotation.z != 0)
         {
-            playerToMove.transform.Rotate(Vector3.forward, -playerToMove.transform.rotation.z);
+            Vector3 eulerRotation = playerToMove.transform.rotation.eulerAngles;
+            playerToMove.transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, 0);
         }
 
     }
