9e4cb5fa4ef82fd2feeeb34546fe0b46bfa73021
SimonL9889 <113265728+SimonL9889@users.noreply.github.com>
2024-02-26 13:11:37 -0500

    partially fix camera

--- Assets/Scripts/Player/CameraRotate.cs
+++ Assets/Scripts/Player/CameraRotate.cs
@@ -16,9 +16,20 @@ public class CameraRotate : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 mousePos = this.input.ReadValue<Vector2>();
-        mousePos *= sensitivity;
-        Quaternion deltaRotation = Quaternion.Euler(/*-mousePos.y*/0f, mousePos.x, 0f);
+        float x = mousePos.x * sensitivity * Time.deltaTime;
+        float y = mousePos.y * sensitivity * Time.deltaTime;
+
+        float verticleRotation = -y;
+        verticleRotation = Mathf.Clamp(verticleRotation, -90f, 90f);
+
+        Quaternion deltaRotation = Quaternion.Euler(sensitivity * verticleRotation/*0f*/, sensitivity * x, 0f);
         Quaternion target = playerToMove.transform.rotation * deltaRotation;
         playerToMove.transform.rotation = Quaternion.RotateTowards(playerToMove.transform.rotation, target, sensitivity);
+
+        if (playerToMove.transform.rotation.z != 0)
+        {
+            playerToMove.transform.Rotate(Vector3.forward, -playerToMove.transform.rotation.z);
+        }
+
     }
 }
